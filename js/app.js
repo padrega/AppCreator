f9bc6feff64ba7fdb828833f0b574ac2b5f07aeb
padrega <paulo.adrega@gmail.com>
2015-03-18 22:52:08 +0000

    demo with jquery.dom-outline

---
+++ js/app.js
@@ -0,0 +1,5 @@
+﻿var myExampleClickHandler = function (element) { console.log('Clicked element:', element); }
+var myDomOutline = DomOutline({ onClick: myExampleClickHandler, filter: '' });
+
+// Start outline:
+myDomOutline.start();
